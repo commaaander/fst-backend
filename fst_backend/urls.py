8aa489c59d3f58816130afceee4ff49f9645bb66
Robert Frießleben <robert.friessleben@shd-online.de>
2023-08-22 13:20:13 +0200

    rename api entry point

--- fst_backend/urls.py
+++ fst_backend/urls.py
@@ -24,7 +24,7 @@ router.register(r"groups", views.GroupViewSet)
 
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("api/v1/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("admin/", admin.site.urls),
 ]
