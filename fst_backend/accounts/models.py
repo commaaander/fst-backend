1e734818c7ac950e947439a45b51f852795db6f8
Robert Frießleben <robert.friessleben@shd-online.de>
2023-08-22 16:55:04 +0200

    add link to Member for CustomUser

--- fst_backend/accounts/models.py
+++ fst_backend/accounts/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 import uuid
+from fst_backend.api.models import Member
 
 
 class CustomUser(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    member = models.OneToOneField(null=True, blank=True, to=Member, on_delete=models.SET_NULL)
