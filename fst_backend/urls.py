2e49ca1e18c03bef223b4bc26c799d945a11cbc6
Robert Frießleben <robert.friessleben@gmail.com>
2023-08-23 20:06:51 +0200

    deactivate drf_spectacular
    see: https://github.com/encode/django-rest-framework/pull/8591

--- fst_backend/urls.py
+++ fst_backend/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from fst_backend.api import views
-from drf_spectacular.views import SpectacularAPIView
+
+# from drf_spectacular.views import SpectacularAPIView
 
 router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
@@ -34,5 +35,5 @@ urlpatterns = [
     path("api/v1/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("admin/", admin.site.urls),
-    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
 ]
