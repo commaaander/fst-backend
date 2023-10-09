1fa52d3ded474306c3ff9f02fde1f6639ab8f35e
Robert Frießleben <robert.friessleben@gmail.com>
2023-10-09 08:01:30 +0200

    add Viewset for CustomDate

--- fst_backend/api/views.py
+++ fst_backend/api/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import Group
 from fst_backend.accounts.models import CustomUser
-from .models import Event, EventMedia, Tag, Member, Allergy
+from .models import Event, EventMedia, Tag, Member, Allergy, CustomDate
 from rest_framework import viewsets
 from rest_framework import permissions
 from .serializers import (
@@ -11,45 +11,36 @@ from .serializers import (
     EventMediaSerializer,
     MemberSerializer,
     AllergySerializer,
+    CustomDateSerializer,
 )
 from rest_framework.parsers import MultiPartParser, FormParser
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-
     queryset = CustomUser.objects.all().order_by("-date_joined")
     serializer_class = CustomUserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 
 class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 
 class EventViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows Events to be viewed or edited.
-    """
-
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 
-class EventMediaViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows EventMedia to be viewed or edited.
-    """
+class CustomDateViewSet(viewsets.ModelViewSet):
+    queryset = CustomDate.objects.all()
+    serializer_class = CustomDateSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
+
+class EventMediaViewSet(viewsets.ModelViewSet):
     queryset = EventMedia.objects.all()
     serializer_class = EventMediaSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -57,30 +48,18 @@ class EventMediaViewSet(viewsets.ModelViewSet):
 
 
 class TagViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows Tags to be viewed or edited.
-    """
-
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 
 class MemberViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows Members to be viewed or edited.
-    """
-
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 
 class AllergyViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows Allergies to be viewed or edited.
-    """
-
     queryset = Allergy.objects.all()
     serializer_class = AllergySerializer
     permission_classes = [permissions.IsAuthenticated]
