c89568705369e84dd44ddc3313f7d947aa3451b8
Robert Frießleben <robert.friessleben@gmail.com>
2023-10-16 20:58:08 +0200

    add base class

--- fst_backend/api/models/event.py
+++ fst_backend/api/models/event.py
@@ -1,13 +1,13 @@
 from .member import Member
+from .base import BaseModel
+
 from django.db import models
-import uuid
 
 from .customdate import CustomDate
 from .tag import Tag
 
 
-class Event(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+class Event(BaseModel):
     title = models.CharField(max_length=64)
     description = models.TextField(blank=True, null=True, max_length=64)
     from_date = models.ForeignKey(
