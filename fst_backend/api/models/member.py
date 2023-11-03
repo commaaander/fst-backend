55ff684dec603131582ea64cb54c7cb1b4a9af10
Robert Frießleben <robert.friessleben@gmail.com>
2023-11-03 15:37:23 +0100

    set better on_delete constraint
    prevents, that a person is deleted if a custom _date is deleted

--- fst_backend/api/models/member.py
+++ fst_backend/api/models/member.py
@@ -9,25 +9,44 @@ from .titlemixin import TitleMixin
 
 
 class Member(BaseModel, TitleMixin, GenderMixin, DietTypeMixin):
-    firstname = models.CharField(blank=True, null=True, max_length=100)
+    # name data
     lastname = models.CharField(max_length=100)
     middlenames = models.CharField(blank=True, null=True, max_length=100)
+    firstname = models.CharField(blank=True, null=True, max_length=100)
+
+    # birthday data
+    birthday = models.ForeignKey(
+        CustomDate,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="birthday_members",
+    )
     birthname = models.CharField(blank=True, null=True, max_length=100)
+    placeOfBirth = models.CharField(blank=True, null=True, max_length=100)
+
+    # deathday data
+    deathday = models.ForeignKey(
+        CustomDate,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="deathday_members",
+    )
+    placeOfDeath = models.CharField(blank=True, null=True, max_length=100)
+
+    # adress
     address = models.CharField(blank=True, null=True, max_length=100)
     zip = models.CharField(blank=True, null=True, max_length=10)
     city = models.CharField(blank=True, null=True, max_length=100)
     country = models.CharField(blank=True, null=True, max_length=100)
+
+    # contact
     email = models.EmailField(blank=True, null=True, max_length=100)
     phone = models.CharField(blank=True, null=True, max_length=100)
+
+    # health data
     allergies = models.ManyToManyField(to=Allergy, blank=True)
-    birthday = models.ForeignKey(
-        CustomDate, blank=True, null=True, on_delete=models.CASCADE, related_name="birthday_members"
-    )
-    deathday = models.ForeignKey(
-        CustomDate, blank=True, null=True, on_delete=models.CASCADE, related_name="deathday_members"
-    )
-    placeOfBirth = models.CharField(blank=True, null=True, max_length=100)
-    placeOfDeath = models.CharField(blank=True, null=True, max_length=100)
 
     def __str__(self) -> str:
-        return f"{self.lastname}, {self.middlenames} {self.firstname}"
+        return f"{self.lastname}, {self.firstname}"
