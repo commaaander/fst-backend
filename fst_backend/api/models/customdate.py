14123d17931be48d0d3c5aad61ee7a8b582cb132
Robert Frießleben <robert.friessleben@gmail.com>
2023-11-03 09:38:13 +0100

    improve date validation

--- fst_backend/api/models/customdate.py
+++ fst_backend/api/models/customdate.py
@@ -2,29 +2,27 @@ from django.db import models
 import uuid
 
 from django.utils.translation import gettext_lazy as _
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.core.exceptions import ValidationError
-from datetime import datetime
+from datetime import date
 
 
 class CustomDate(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    day = models.PositiveIntegerField(blank=True, null=True)
-    month = models.PositiveIntegerField(blank=True, null=True)
-    year = models.PositiveIntegerField(blank=True, null=True)
+    day = models.PositiveIntegerField(blank=True, null=True, validators=[MinValueValidator(1), MaxValueValidator(31)])
+    month = models.PositiveIntegerField(
+        blank=True, null=True, validators=[MinValueValidator(1), MaxValueValidator(12)]
+    )
+    year = models.PositiveIntegerField(
+        blank=True, null=True, validators=[MinValueValidator(1), MaxValueValidator(9999)]
+    )
 
     def clean(self):
-        # Check if day, month, and year are present and form a valid date.
         if self.day and self.month:
             try:
-                # If the year isn't set, use a leap year to allow for February 29th.
-                datetime(year=self.year or 2000, month=self.month, day=self.day)
+                date(year=self.year or 2000, month=self.month, day=self.day)
             except ValueError:
-                raise ValidationError(
-                    _("Invalid date: {year:04d}-{month:02d}-{day:02d}").format(
-                        year=self.year or 0, month=self.month or 0, day=self.day or 0
-                    )
-                )
+                raise ValidationError(_("Invalid date: Month {:02d} has no day {:02d}").format(self.month, self.day))
 
     def __str__(self):
-        return "{year:04d}-{month:02d}-{day:02d}".format(year=self.year or 0, month=self.month or 0, day=self.day or 0)
+        return "{:04d}-{:02d}-{:02d}".format(self.year or 0, self.month or 0, self.day or 0)
