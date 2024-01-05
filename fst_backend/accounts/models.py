76dc859bf5d60ff8a7e2fb87078cad5ff1f7495c
Robert Frießleben <robert.friessleben@gmail.com>
2024-01-05 16:15:06 +0100

    Update CustomUser model to use Person instead of Member

--- fst_backend/accounts/models.py
+++ fst_backend/accounts/models.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 import uuid
-from fst_backend.api.models import Member
+from fst_backend.api.models import Person
 
 
 class CustomUser(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    member = models.OneToOneField(null=True, blank=True, to=Member, on_delete=models.SET_NULL)
+    person = models.OneToOneField(null=True, blank=True, to=Person, on_delete=models.SET_NULL)
