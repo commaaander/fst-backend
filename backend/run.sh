211aa419d471ed5fd02daaed8788f1547d1cff6f
Robert Frießleben <robert.friessleben@gmail.com>
2024-01-07 10:22:53 +0100

    Add makemigrations command to run.sh

--- backend/run.sh
+++ backend/run.sh
@@ -4,6 +4,7 @@ set -ex
 
 python manage.py wait_for_db
 # python manage.py collectstatic --noinput
+python manage.py makemigrations
 python manage.py migrate
 python manage.py create_superuser
 python manage.py create_initial_db_content
