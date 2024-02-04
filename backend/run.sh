3fcf70b71814cbf24daf39e8ae4bcf1c455f9a29
Robert Frießleben <robert.friessleben@gmail.com>
2024-02-04 20:31:05 +0100

    add remote debugging in container, update packages

--- backend/run.sh
+++ backend/run.sh
@@ -8,4 +8,8 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py create_superuser
 python manage.py create_initial_db_content
-python manage.py runserver 0.0.0.0:8000
+if [ "$DJANGO_DEBUG" = "1" ]; then
+    python -m debugpy --listen 0.0.0.0:3000 manage.py runserver 0.0.0.0:8000
+else 
+    python manage.py runserver 0.0.0.0:8000
+fi
